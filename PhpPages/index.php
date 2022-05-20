bb54f2120108f22be6ed15f5175518fd305cfec2
Kareem Ali <kareemsharks593@gmail.com>
2022-05-20 19:26:36 +0200

    Update

--- PhpPages/index.php
+++ PhpPages/index.php
@@ -1,58 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-    <style>
-
-        table,th,td{
-            font-family: arial;
-            border: 1px solid;
-            text-align: center;
-            padding: 5px;
-        }
-
-        .center {
-        margin-left: auto;
-        margin-right: auto;
-        border: 4px solid darkblue;
-        border-radius: 5px;
-        border-style: inset;
-        }
-
-        h1{
-            text-align:center;
-            font-family:arial;
-        }
-        
-
-        .UserForm{
-            margin:10px;
-            text-align:center;
-        }
-
-        a{
-            margin:10px;
-        }
-        body{
-            background-color:rgb(201, 225, 247);
-        }
-    </style>
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="../View Pages/Users.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
 
-    <h1>List All Users</h1>
 
-    <table class="center">
+<div class="center">
+    <h1> List All Users </h1>
+<table class="#">
+    
     <?php 
     include '../Classes/User.php';
     $User1 = new User("../TextFiles/Users.txt","~");
     $User2 = $User1->ListAllUsers();
     
 
+    
     for($i=0;$i<count($User2);$i++){
         echo "<tr>"; 
             echo "<td>".$User2[$i]->getId()."</td>";
@@ -70,11 +38,13 @@
     
     
     <td>
-    <a href="AddUser.html">Add User</a>
+    <a href="../View Pages/AddUser.html">Add User</a>
     </td>
 
    </table>
 
+</div>
+    
 
 
 </body>
